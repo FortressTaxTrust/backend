bc9df23b46227d35748241115c55b67efd4f016f
Safee36 <muhammadsafee5643@gmail.com>
2025-11-21 00:40:48 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -51,7 +51,9 @@ app.get('/health', (_req, res) => res.status(200).send('OK'));
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "200mb" }));
+app.use(express.urlencoded({ limit: "200mb", extended: true }));
+
 
 // Root route
 app.get('/', (req, res) => {
